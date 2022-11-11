d232d7ac648d1a45ac80a5de2e1a342cef299c32
theMackabu <theMackabu@gmail.com>
2022-11-10 18:40:31 -0800

    fix link err

--- testing/test.js
+++ testing/test.js
@@ -1,7 +1,7 @@
 core.print('\x1b[35mcore.uuid() \x1b[0m');
 console.log(core.uuid());
 
-core.print(`\x1b[35mhttp.get('https://httpbin.org/get') \x1b[0m`);
+core.print(`\x1b[35mhttp.get('https://api.github.com/users/theMackabu/repos') \x1b[0m`);
 let json = await http.get('https://httpbin.org/get').then((data) => data.json());
 
 console.log(json.headers['Host']);
