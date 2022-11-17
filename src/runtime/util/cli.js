55be361295a6e451ccac3a8d6c419ce73497c182
theMackabu <theMackabu@gmail.com>
2022-11-16 21:09:49 -0800

    cli colors

--- src/runtime/util/cli.js
+++ src/runtime/util/cli.js
@@ -2,6 +2,53 @@
 	const { core } = Deno;
 	const { ops } = core;
 
+	const format = {
+		system: {
+			reset: '\x1b[0m',
+			bold: '\x1b[1m',
+			dim: '\x1b[2m',
+			italic: '\x1b[3m',
+			underscore: '\x1b[4m',
+			reverse: '\x1b[7m',
+			strikethrough: '\x1b[9m',
+			backoneline: '\x1b[1A',
+			cleanthisline: '\x1b[K',
+			clear: '\033[2J',
+		},
+		font: {
+			black: '\x1b[30m',
+			red: '\x1b[31m',
+			green: '\x1b[32m',
+			yellow: '\x1b[33m',
+			blue: '\x1b[34m',
+			magenta: '\x1b[35m',
+			cyan: '\x1b[36m',
+			white: '\x1b[37m',
+		},
+		bg: {
+			black: '\x1b[40m',
+			red: '\x1b[41m',
+			green: '\x1b[42m',
+			yellow: '\x1b[43m',
+			blue: '\x1b[44m',
+			magenta: '\x1b[45m',
+			cyan: '\x1b[46m',
+			white: '\x1b[47m',
+		},
+	};
+
+	const convertHex = (hex) => {
+		var c;
+		if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+			c = hex.substring(1).split('');
+			if (c.length == 3) {
+				c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+			}
+			c = '0x' + c.join('');
+			return [(c >> 16) & 255, (c >> 8) & 255, c & 255];
+		}
+	};
+
 	const formatChain = (string, format = false) =>
 		JSON.stringify(string, null, format ? 2 : 0)
 			.replace(/\\n/g, '\n')
@@ -18,6 +65,9 @@
 	};
 
 	globalThis.console = {
+		format: format.system,
+		color: format.font,
+		bg: format.bg,
 		log: (...args) => {
 			ops.op_stdout(logWithoutObject(...args));
 		},
@@ -30,8 +80,44 @@
 		error: (...args) => {
 			ops.op_stderr(logWithoutObject(...args));
 		},
+		fmt: {
+			print: (color, string) => {
+				ops.op_print(color);
+				ops.op_print(logWithoutObject(string));
+				ops.op_print(format.system.reset);
+			},
+			println: (color, string) => {
+				ops.op_print(color);
+				ops.op_stdout(logWithoutObject(string));
+				ops.op_print(format.system.reset);
+			},
+			rgb: {
+				print: (r = 255, g = 255, b = 255, string) => {
+					ops.op_print(`\x1b[38;2;${r};${g};${b}m`);
+					ops.op_print(logWithoutObject(string));
+					ops.op_print(format.system.reset);
+				},
+				println: (r = 255, g = 255, b = 255, string) => {
+					ops.op_print(`\x1b[38;2;${r};${g};${b}m`);
+					ops.op_stdout(logWithoutObject(string));
+					ops.op_print(format.system.reset);
+				},
+			},
+			hex: {
+				print: (hex = '#ffffff', string) => {
+					ops.op_print(`\x1b[38;2;${convertHex(hex)[0].toString()};${convertHex(hex)[1].toString()};${convertHex(hex)[2].toString()}m`);
+					ops.op_print(logWithoutObject(string));
+					ops.op_print(format.system.reset);
+				},
+				println: (hex = '#ffffff', string) => {
+					ops.op_print(`\x1b[38;2;${convertHex(hex)[0].toString()};${convertHex(hex)[1].toString()};${convertHex(hex)[2].toString()}m`);
+					ops.op_stdout(logWithoutObject(string));
+					ops.op_print(format.system.reset);
+				},
+			},
+		},
 		clear: () => {
-			ops.op_stdout('\033[2J\033[1;1H');
+			ops.op_stdout(format.system.clear);
 		},
 	};
 })(globalThis);
--- tests/console.js
+++ tests/console.js
@@ -3,3 +3,30 @@ console.log(1, 1);
 console.log(1, 'hello');
 console.log('hello', 1);
 console.log('hello', 1, 'world');
+
+core.print('\n');
+
+console.fmt.println(console.color.red, 'red');
+console.fmt.println(console.color.yellow, 'yellow');
+console.fmt.println(console.color.green, 'green');
+console.fmt.println(console.color.cyan, 'cyan');
+console.fmt.println(console.color.blue, 'blue');
+console.fmt.println(console.color.magenta, 'magenta');
+
+core.print('\n');
+
+console.fmt.print(console.color.red, 'red ');
+console.fmt.print(console.color.yellow, 'yellow ');
+console.fmt.print(console.color.green, 'green ');
+console.fmt.print(console.color.cyan, 'cyan ');
+console.fmt.print(console.color.blue, 'blue ');
+console.fmt.println(console.color.magenta, 'magenta ');
+
+core.print('\n');
+
+console.fmt.hex.println('#E50104', '███████████████████');
+console.fmt.hex.println('#FE8B00', '███████████████████');
+console.fmt.hex.println('#FEED00', '███████████████████');
+console.fmt.hex.println('#008026', '███████████████████');
+console.fmt.hex.println('#004CFF', '███████████████████');
+console.fmt.hex.println('#750786', '███████████████████');
