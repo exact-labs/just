8e2692a6107bcb6852cd7f32002697d4c1ae33c7
theMackabu <theMackabu@gmail.com>
2022-11-28 19:38:49 -0800

    release v0.4.0

--- src/runtime/util/ext.js
+++ src/runtime/util/ext.js
@@ -3,7 +3,7 @@
 	const { ops } = core;
 
 	globalThis.ext = {
-		raw: (raw) => core.run_ext_func(raw),
+		raw: (raw) => ops.run_ext_func(raw),
 		file: {
 			read: (path) => ops.run_ext_func('get_file:' + path),
 		},
